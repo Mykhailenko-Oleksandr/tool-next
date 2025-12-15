00b6d90a6075abaf6dfde786d0ef9dc4b6c65528
camarro84 <camarro84@gmail.com>
2025-12-15 20:23:01 +0100

    only-toolgrid-task17

---
+++ components/ToolGrid/ToolGrid.tsx
@@ -0,0 +1,17 @@
+import css from "./ToolGrid.module.css";
+import ToolCard from "../ToolCard/ToolCard";
+import { Tool } from "@/types/tool";
+
+type Props = {
+  tools: Tool[];
+};
+
+export default function ToolGrid({ tools }: Props) {
+  return (
+    <div className={css.grid}>
+      {tools.map((tool) => (
+        <ToolCard key={tool._id} tool={tool} />
+      ))}
+    </div>
+  );
+}
