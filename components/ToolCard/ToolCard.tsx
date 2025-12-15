0a63df93cbfcd122fb55e520f42242604e20f4c0
Sapper <saniamykhailenko@gmail.com>
2025-12-15 14:06:27 +0200

    add btns

--- components/ToolCard/ToolCard.tsx
+++ components/ToolCard/ToolCard.tsx
@@ -2,25 +2,54 @@ import Image from "next/image";
 import Link from "next/link";
 import css from "./ToolCard.module.css";
 import StarsRating from "../StarsRating/StarsRating";
+import { Tool } from "@/types/tool";
 
-export default function ToolCard() {
+interface ToolCardProps {
+  tool: Tool;
+}
+
+export default function ToolCard({ tool }: ToolCardProps) {
+  const isAuth = false;
+
   return (
     <>
       <div className={css.toolCard}>
         <Image
-          src="/images/blank-image-mob.jpg"
-          alt="Tool image"
+          src={tool.images}
+          alt={tool.name}
           width={335}
           height={413}
           className={css.image}
         />
 
-        <StarsRating rating={3.3} />
-        <h4 className={css.title}>Штукатурна станція PFT G4</h4>
-        <p className={css.price}>1500 грн/день</p>
-        <Link className={css.link} href="/tools/id">
-          Детальніше
-        </Link>
+        <StarsRating rating={tool.rating} />
+        <h4 className={css.title}>{tool.name}</h4>
+        <p className={css.price}>`{tool.pricePerDay} грн/день`</p>
+        {isAuth ? (
+          <Link
+            className={css.link}
+            href={`/tools/${tool.id}`}>
+            Детальніше
+          </Link>
+        ) : (
+          <div className={css.btnBox}>
+            <Link
+              className={css.link}
+              href={`/tools/edit/${tool.id}`}>
+              Редагувати
+            </Link>
+            <button
+              className={css.deleteBtn}
+              type="button">
+              <svg
+                className={css.icon}
+                width={24}
+                height={24}>
+                <use href="/icons.svg#icon-delete"></use>
+              </svg>
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
