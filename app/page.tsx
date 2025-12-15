ec08a44f513fc96e5c6961b99d38ea0a6773e55e
Michael <captkit@ukr.net>
2025-12-15 17:37:59 +0200

    Add FeaturedToolsBlock component and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import css from "./Home.module.css";
+// app/page.tsx
 import Benefits from "@/components/Benefits/Benefits";
+import FeaturedToolsBlock from "@/components/FeaturedToolsBlock/FeaturedToolsBlock";
 
-export default function Home() {
+export default async function HomePage() {
+  // перший запит до API (серверна частина)
+  const res = await fetch("http://localhost:3000/api/tools?page=1", {
+    cache: "no-store", // щоб завжди брати свіжі дані
+  });
+
+  // const tools = await res.json();
+
+  // Захист від HTML-404, щоб не падати на res.json()
+  const tools = res.ok ? await res.json() : [];
+
   return (
-    <>
+    <main>
       <Benefits />
-    </>
+      <FeaturedToolsBlock tools={tools} />
+    </main>
   );
 }
