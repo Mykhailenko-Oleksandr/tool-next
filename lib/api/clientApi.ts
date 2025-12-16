db170a38ebc1cb681451bc7e0d03761df2506760
Kateryna Balashova <katybalashov@gmail.com>
2025-12-16 18:35:26 +0200

    create login page

---
+++ lib/api/clientApi.ts
@@ -0,0 +1,12 @@
+import { User } from "@/types/user";
+import { nextServer } from "./api";
+
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export const login = async (data: LoginRequest) => {
+  const res = await nextServer.post<User>('/auth/login', data);
+  return res.data;
+};
