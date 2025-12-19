5d9e1456d58162005c0aa5455d693e6d543ddd88
Sapper <saniamykhailenko@gmail.com>
2025-12-19 11:54:30 +0200

    start task. add title

--- app/(private routes)/tools/[toolId]/booking/page.tsx
+++ app/(private routes)/tools/[toolId]/booking/page.tsx
@@ -1,3 +1,33 @@
-export default function Foo() {
-  return <></>;
+import { fetchToolById } from "@/lib/api/clientApi";
+import { Metadata } from "next";
+import css from "./BookingTool.module.css";
+
+interface Props {
+  params: Promise<{ toolId: string }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { toolId } = await params;
+  const tool = await fetchToolById(toolId);
+
+  return {
+    title: `Бронювання ${tool.name}`,
+    description: tool.description.slice(0, 30),
+    openGraph: {
+      title: `Бронювання ${tool.name}`,
+      description: tool.description.slice(0, 100),
+      url: `https://tool-next-chi.vercel.app/tools/${toolId}/booking`,
+      images: [{ url: tool.images }],
+    },
+  };
+}
+
+export default function BookingTool() {
+  return (
+    <section className={css.section}>
+      <div className="container">
+        <h2 className={css.title}>Підтвердження бронювання</h2>
+      </div>
+    </section>
+  );
 }
