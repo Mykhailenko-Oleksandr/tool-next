b3cb07aea6d098a9e598fbe174a0855133866939
Sapper <saniamykhailenko@gmail.com>
2025-12-22 06:42:36 +0200

    add meta new

--- app/(other)/layout.tsx
+++ app/(other)/layout.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import { ReactNode } from "react";
 
-export default function RootLayout({
+export default function OtherLayout({
   children,
 }: Readonly<{
   children: ReactNode;
--- app/(other)/page.tsx
+++ app/(other)/page.tsx
@@ -4,6 +4,24 @@ import FeedbacksBlock from "@/components/FeedbacksBlock/FeedbacksBlock";
 import Hero from "@/components/Hero/Hero";
 import RegistrationBlock from "@/components/RegistrationBlock/RegistrationBlock";
 import { fetchPopularTool } from "@/lib/api/clientApi";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Створення інструмента",
+  description:
+    "Створи оголошення про оренду інструмента на ToolNext. Додай фото, опис, характеристики та ціну — і почни заробляти на своєму обладнанні вже сьогодні. Швидко, зручно та без зайвих кроків.",
+  openGraph: {
+    title: "Створення інструмента — ToolNext",
+    description:
+      "Додай інструмент для оренди на ToolNext. Просте створення оголошення, керування орендою та стабільний дохід від твого обладнання.",
+    url: "https://tool-next-chi.vercel.app/tools/new",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddln4hnns/image/upload/v1765352917/cover_kkf3m7.jpg",
+      },
+    ],
+  },
+};
 
 export default async function HomePage() {
   const res = await fetchPopularTool();
