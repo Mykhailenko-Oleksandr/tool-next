08f822f9a0d831939d2e2035f47578d204267ace
Michael <captkit@ukr.net>
2025-12-22 03:38:58 +0200

    booking-css revision 2

--- components/BookingToolForm/BookingToolForm.tsx
+++ components/BookingToolForm/BookingToolForm.tsx
@@ -14,10 +14,8 @@ import { ApiError } from "@/app/api/api";
 function getDaysCount(startDate: string, endDate: string): number {
   const start = new Date(startDate);
   const end = new Date(endDate);
-
   const diffTime = end.getTime() - start.getTime();
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
   return diffDays || 1;
 }
 
@@ -46,10 +44,8 @@ const BookingSchema = Yup.object().shape({
     .required("Оберіть дату завершення")
     .when("startDate", ([startDate], schema) => {
       if (!startDate) return schema;
-
       const minDate = new Date(startDate);
       minDate.setDate(minDate.getDate() + 1);
-
       return schema.min(
         minDate,
         "Дата завершення має бути пізніше за дату початку"
@@ -71,12 +67,7 @@ export default function BookingToolForm({ tool }: Props) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const key = e.target.name;
     const value = e.target.value;
-
-    const changeEl = {
-      ...draft,
-      [key]: value,
-    };
-    setDraft(changeEl);
+    setDraft({ ...draft, [key]: value });
   };
 
   const handleSubmit = async (
@@ -90,7 +81,6 @@ export default function BookingToolForm({ tool }: Props) {
       router.push("/confirm/booking");
     } catch (error: unknown) {
       const err = error as ApiError;
-
       toast.error(
         err.response?.data?.response?.validation?.body?.message ||
           err.response?.data?.response?.message ||
@@ -116,138 +106,163 @@ export default function BookingToolForm({ tool }: Props) {
               )
             : 0;
         return (
-          <Form className={css.form}>
-            <fieldset className={css.fieldset}>
-              <label htmlFor={`${fieldId}-firstName`} className={css.label}>
-                Ім&apos;я
-              </label>
-              <Field
-                type="text"
-                name="firstName"
-                placeholder="Ваше ім'я"
-                id={`${fieldId}-firstName`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="firstName"
-                component="span"
-                className={css.error}
-              />
-              <label htmlFor={`${fieldId}-lastName`} className={css.label}>
-                Прізвище
-              </label>
-              <Field
-                type="text"
-                name="lastName"
-                placeholder="Ваше прізвище"
-                id={`${fieldId}-lastName`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="lastName"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
-            <fieldset className={css.fieldset}>
-              <label htmlFor={`${fieldId}-phone`} className={css.label}>
-                Номер телефону
-              </label>
-              <Field
-                type="tel"
-                name="phone"
-                placeholder="+38 (XXX) XXX XX XX"
-                id={`${fieldId}-phone`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="phone"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
+          <div className={css.wrapper}>
+            <Form className={css.form}>
+              {/* Ім’я + Прізвище */}
+              <fieldset className={css.fieldset}>
+                <div className={css.control}>
+                  <label htmlFor={`${fieldId}-firstName`} className={css.label}>
+                    Ім&apos;я
+                  </label>
+                  <Field
+                    type="text"
+                    name="firstName"
+                    placeholder="Ваше ім'я"
+                    id={`${fieldId}-firstName`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="firstName"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+                <div className={css.control}>
+                  <label htmlFor={`${fieldId}-lastName`} className={css.label}>
+                    Прізвище
+                  </label>
+                  <Field
+                    type="text"
+                    name="lastName"
+                    placeholder="Ваше прізвище"
+                    id={`${fieldId}-lastName`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="lastName"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+              </fieldset>
 
-            <fieldset className={css.fieldset}>
-              <label htmlFor={`${fieldId}-startDate`} className={css.label}>
-                Дата початку
-              </label>
-              <Field
-                type="date"
-                name="startDate"
-                id={`${fieldId}-startDate`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="startDate"
-                component="span"
-                className={css.error}
-              />
-              <label htmlFor={`${fieldId}-endDate`} className={css.label}>
-                Дата завершення
-              </label>
-              <Field
-                type="date"
-                name="endDate"
-                id={`${fieldId}-endDate`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="endDate"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
+              {/* Телефон */}
+              <fieldset className={`${css.fieldset} ${css.single}`}>
+                <div className={css.control}>
+                  <label htmlFor={`${fieldId}-phone`} className={css.label}>
+                    Номер телефону
+                  </label>
+                  <Field
+                    type="tel"
+                    name="phone"
+                    placeholder="+38 (XXX) XXX XX XX"
+                    id={`${fieldId}-phone`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="phone"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+              </fieldset>
 
-            <fieldset className={css.fieldset}>
-              <label htmlFor={`${fieldId}-deliveryCity`} className={css.label}>
-                Місто доставки
-              </label>
-              <Field
-                type="text"
-                name="deliveryCity"
-                placeholder="Ваше місто"
-                id={`${fieldId}-deliveryCity`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="deliveryCity"
-                component="span"
-                className={css.error}
-              />
-              <label
-                htmlFor={`${fieldId}-deliveryBranch`}
-                className={css.label}
-              >
-                Відділення Нової Пошти
-              </label>
-              <Field
-                type="text"
-                name="deliveryBranch"
-                placeholder="24"
-                id={`${fieldId}-deliveryBranch`}
-                className={css.input}
-                onChange={handleChange}
-              />
-              <ErrorMessage
-                name="deliveryBranch"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
+              {/* Дати */}
+              <fieldset className={css.fieldset}>
+                <div className={css.control}>
+                  <label htmlFor={`${fieldId}-startDate`} className={css.label}>
+                    Дата початку
+                  </label>
+                  <Field
+                    type="date"
+                    name="startDate"
+                    id={`${fieldId}-startDate`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="startDate"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+                <div className={css.control}>
+                  <label htmlFor={`${fieldId}-endDate`} className={css.label}>
+                    Дата завершення
+                  </label>
+                  <Field
+                    type="date"
+                    name="endDate"
+                    id={`${fieldId}-endDate`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="endDate"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+              </fieldset>
 
-            <div className={css.priceRow}>
-              <p className={css.price}>Вартість: {totalPrice} грн</p>
-              <button type="submit" className={css.submitBtn}>
-                Забронювати
-              </button>
-            </div>
-          </Form>
+              {/* Місто + Відділення */}
+              <fieldset className={css.fieldset}>
+                <div className={css.control}>
+                  <label
+                    htmlFor={`${fieldId}-deliveryCity`}
+                    className={css.label}
+                  >
+                    Місто доставки
+                  </label>
+                  <Field
+                    type="text"
+                    name="deliveryCity"
+                    placeholder="Ваше місто"
+                    id={`${fieldId}-deliveryCity`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="deliveryCity"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+                <div className={css.control}>
+                  <label
+                    htmlFor={`${fieldId}-deliveryBranch`}
+                    className={css.label}
+                  >
+                    Відділення Нової Пошти
+                  </label>
+                  <Field
+                    type="text"
+                    name="deliveryBranch"
+                    placeholder="24"
+                    id={`${fieldId}-deliveryBranch`}
+                    className={css.input}
+                    onChange={handleChange}
+                  />
+                  <ErrorMessage
+                    name="deliveryBranch"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+              </fieldset>
+
+              {/* Ціна + кнопка */}
+              <div className={css.priceRow}>
+                <p className={css.price}>Вартість: {totalPrice} грн</p>
+                <button type="submit" className={css.submitBtn}>
+                  Забронювати
+                </button>
+              </div>
+            </Form>
+          </div>
         );
       }}
     </Formik>
