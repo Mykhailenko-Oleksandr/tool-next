15a034bae23ab3c6b82a155d71c0bfb1c0c5fe59
Sapper <saniamykhailenko@gmail.com>
2025-12-25 23:15:12 +0200

    upd metadata

--- app/(other)/tools/new/page.tsx
+++ app/(other)/tools/new/page.tsx
@@ -1,5 +1,23 @@
 import AddEditToolForm from "@/components/AddEditToolForm/AddEditToolForm";
 import css from "./AddToolPage.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Створення нового інструмента",
+  description:
+    "Створіть та опублікуйте нове оголошення для оренди вашого інструмента",
+  openGraph: {
+    title: "Створення нового інструмента на ToolNext",
+    description:
+      "Створіть та опублікуйте нове оголошення для оренди вашого інструмента на ToolNext. Почніть отримувати пасивний дохід.",
+    url: "https://tool-next-chi.vercel.app/tools/new",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddln4hnns/image/upload/v1765352917/cover_kkf3m7.jpg",
+      },
+    ],
+  },
+};
 
 export default function AddToolPage() {
   return (
