66c30f310b3a5a595995d6d5b0033e51edc9af26
Oleh Butenko <butenkoleh@gmail.com>
2025-12-26 19:40:56 +0200

    add autoscroll to profiles

--- app/(other)/profile/Profile.client.tsx
+++ app/(other)/profile/Profile.client.tsx
@@ -12,58 +12,71 @@ import { Tool } from "@/types/tool";
 import Loader from "@/components/Loader/Loader";
 
 interface ProfileClientProps {
-  user: User;
+	user: User;
 }
 
 export default function ProfileClient({ user }: ProfileClientProps) {
-  const {
-    data,
-    isLoading,
-    isFetchingNextPage,
-    isError,
-    fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery({
-    queryKey: ["tools", user._id],
-    queryFn: ({ pageParam = 1 }) => fetchToolsUserId(user._id, pageParam),
-    getNextPageParam: (lastPage) => {
-      return lastPage.page < lastPage.totalPages
-        ? lastPage.page + 1
-        : undefined;
-    },
-    initialPageParam: 1,
-  });
+	const {
+		data,
+		isLoading,
+		isFetchingNextPage,
+		isError,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery({
+		queryKey: ["tools", user._id],
+		queryFn: ({ pageParam = 1 }) => fetchToolsUserId(user._id, pageParam),
+		getNextPageParam: (lastPage) => {
+			return lastPage.page < lastPage.totalPages
+				? lastPage.page + 1
+				: undefined;
+		},
+		initialPageParam: 1,
+	});
 
-  const tools: Tool[] = data?.pages.flatMap((page) => page.tools) ?? [];
+	const tools: Tool[] = data?.pages.flatMap((page) => page.tools) ?? [];
 
-  return (
-    <section className={css.profilePage}>
-      <div className="container">
-        <UserProfile user={user} />
-        <div className={css.titleWrap}>
-          <h2 className={css.profileToolsTitle}>Інструменти</h2>
-        </div>
+	const handleLoadMore = () => {
+		const currentScrollPosition = window.pageYOffset;
 
-        {!isLoading && tools.length === 0 && <PrivateProfilePlaceholder />}
+		fetchNextPage().then(() => {
+			setTimeout(() => {
+				window.scrollTo({
+					top: currentScrollPosition + 500,
+					behavior: "smooth",
+				});
+			}, 100);
+		});
+	};
 
-        {isLoading && <Loader />}
-        {isError && !data && <p>Щось пішло не так... Спробуйте ще.</p>}
-        {isFetchingNextPage && !isLoading && <Loader />}
+	return (
+		<section className={css.profilePage}>
+			<div className="container">
+				<UserProfile user={user} />
+				<div className={css.titleWrap}>
+					<h2 className={css.profileToolsTitle}>Інструменти</h2>
+				</div>
 
-        {tools.length > 0 && (
-          <>
-            <ToolGrid tools={tools} />
+				{!isLoading && tools.length === 0 && <PrivateProfilePlaceholder />}
 
-            {hasNextPage && (
-              <LoadMoreButton
-                onClick={() => fetchNextPage()}
-                disabled={isFetchingNextPage}
-                loading={isFetchingNextPage}
-              />
-            )}
-          </>
-        )}
-      </div>
-    </section>
-  );
+				{isLoading && <Loader />}
+				{isError && !data && <p>Щось пішло не так... Спробуйте ще.</p>}
+				{isFetchingNextPage && !isLoading && <Loader />}
+
+				{tools.length > 0 && (
+					<>
+						<ToolGrid tools={tools} />
+
+						{hasNextPage && (
+							<LoadMoreButton
+								onClick={handleLoadMore}
+								disabled={isFetchingNextPage}
+								loading={isFetchingNextPage}
+							/>
+						)}
+					</>
+				)}
+			</div>
+		</section>
+	);
 }
