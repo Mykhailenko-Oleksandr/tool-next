9851d46d61eedaf946e9eaa68d66159bd3a52ba4
Sapper <saniamykhailenko@gmail.com>
2025-12-29 09:16:10 +0200

    upd response userById

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -3,6 +3,14 @@ import { nextServer } from "./api";
 import { User } from "@/types/user";
 import { CheckSessionRequest, UserToolsResponse } from "./clientApi";
 
+interface UserByIdResponse {
+  _id: string;
+  name: string;
+  avatarUrl: string;
+  rating: number;
+  feedbacksCount: number;
+}
+
 export async function getMe() {
   const cookieStore = await cookies();
   const { data } = await nextServer.get<User>("/users/me", {
@@ -47,7 +55,7 @@ export async function fetchToolsUserId(
 
 export async function fetchUserById(id: string) {
   const cookieStore = await cookies();
-  const response = await nextServer.get<User>(`/users/${id}`, {
+  const response = await nextServer.get<UserByIdResponse>(`/users/${id}`, {
     headers: {
       Cookie: cookieStore.toString(),
     },
